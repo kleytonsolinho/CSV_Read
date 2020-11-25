79b66b380e447890811b38d977693bd6f67e2879
Kleyton Solinho <kleytonsolinho@gmail.com>
2020-11-25 19:59:32 -0300

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-teste
+const csv = require('csv-parser');
+const fs = require('fs');
+const results = [];
+
+fs.createReadStream('data.csv')
+  .pipe(csv({}))
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+  });
